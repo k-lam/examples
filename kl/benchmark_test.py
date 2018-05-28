580fb966f3867c51e47751911edb7b6f4c6f6a7a
kl <kl@duoweimmm.com>
2018-05-28 21:03:44 +0800

    remove prinf

--- kl/benchmark_test.py
+++ kl/benchmark_test.py
@@ -15,8 +15,6 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 
-print torchvision.__version__
-
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
     and callable(models.__dict__[name]))
