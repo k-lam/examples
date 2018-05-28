bb3d14173452f94835b072203c5e8d085636935a
kl <kl@duoweimmm.com>
2018-05-28 15:28:02 +0800

    add benchmark_test.py

---
+++ kl/benchmark_test.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+import shutil
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+
+model_names = sorted(name for name in models.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Benchmark')
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18',
+                    choices=model_names,
+                    help='model architecture: ' +
+                        ' | '.join(model_names) +
+                        ' (default: resnet18)')
+
+
+
+def main():
+    args = parser.parse_args()
+    valdir = os.path.join(args.data, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+    model = models.__dict__[args.arch]
+    checkpoint = torch.load(args.arch+'.ckpt')
+    model.load_state_dict(checkpoint['state_dict'])
+    model.cuda()
+    model.eval()
+    s = time.time()
+    len = len(val_loader)
+    for i, (input, target) in enumerate(val_loader):
+        print i, '-----------------------------'
+        model(input)
+    e = time.time()
+
+    print 'total cost:', e - s, 'image count:', len, 'avg:', (e - s) / len
